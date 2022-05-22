814ade3f2db6d7b83dd1e5bfd655e0e08bb7e153
Leongard <vovchick2210@gmail.com>
2022-05-22 22:34:41 +0800

    spring boot init

---
+++ src/main/java/com/example/moominvalley/MoominvalleyApplication.java
@@ -0,0 +1,13 @@
+package com.example.moominvalley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoominvalleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoominvalleyApplication.class, args);
+	}
+
+}
